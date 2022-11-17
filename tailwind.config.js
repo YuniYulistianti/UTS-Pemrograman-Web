f7564ecf62720863793c2929c87476d3f9c46728
YuniYulistianti <116367600+YuniYulistianti@users.noreply.github.com>
2022-11-17 12:59:50 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          primary: '#FF994F;',
+        },
+        fontFamily: {
+          base: ['Nunito', 'sans-serif'],
+          nav: ['Raleway', 'sans-serif'],
+        },
+        fontSize: {
+          'heading-1': '50px',
+        },
+      },
+    },
+  };
